8246c6b225f6fe642a0570ceeba640ce1efa7f2a
adithyan7053 <adithyan7053@gmail.com>
2025-11-01 16:45:49 +0530

    updated services offered

--- src/components/form/dropdowns/AdditionalTypesDropdown.tsx
+++ src/components/form/dropdowns/AdditionalTypesDropdown.tsx
@@ -14,30 +14,10 @@ import {
 import { Button } from "@/components/ui/button";
 import { ChevronDown } from "lucide-react";
 import { useEffect, useState } from "react";
+import { SERVICE_OPTIONS } from "@/constants";
 
 // Define service options
-const SERVICE_OPTIONS = [
-  {
-    typeId: "429e89be-7a73-40d3-b149-a9c62b531d6b",
-    label: "Car with Driver",
-    value: "car-with-driver",
-  },
-  {
-    typeId: "1373249a-9fca-4387-b6a3-1decc434e726",
-    label: "Airport Pickup",
-    value: "airport-pickup",
-  },
-  {
-    typeId: "de553fb6-84a5-46a4-8e27-71640dfb7b74",
-    label: "Monthly Rentals",
-    value: "monthly-rentals",
-  },
-  {
-    typeId: "029703ed-928a-4cf0-8c0e-d160670b12da",
-    label: "Self Driving",
-    value: "self-driving",
-  },
-];
+
 
 type AdditionalTypesDropdownProps = {
   value: string[];
