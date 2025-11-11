b9b1d26a98ffb7293027ad68bde0610d9848afa4
Ajesh <ajeshs.dev@gmail.com>
2025-11-11 23:18:04 +0530

    feat: Price Offer enabled in live listing table

--- src/api/Api-Endpoints.ts
+++ src/api/Api-Endpoints.ts
@@ -98,6 +98,7 @@ export enum Slug {
   GET_NEW_OR_MODIFIED_VEHICLES = "/vehicle/all/modified",
   PUT_TOGGLE_VEHICLE_VISIBILITY = "/vehicle/toggle-visibility",
   POST_TOGGLE_VEHICLE_PRIORITY = "/vehicle/high-priority-vehicle",
+  PUT_TOGGLE_VEHICLE_PRICE_OFFER = "/vehicle/toggle-price-offer",
   GET_LEVELS_FILLED = "/vehicle/level-filled",
   GET_VEHICLE_LISTINGS_COUNT_BY_STATE = "/vehicle/dashboard/count",
   GET_ALL_VEHICLE_LISTINGS_COUNT = "/vehicle/dashboard/count/state",
@@ -161,8 +162,8 @@ export enum Slug {
   GET_SUPPLIER_CENTRAL_HOME_SUMMARY = "/supplier-central/home-summary",
   GET_SUPPLIER_CENTRAL_CATEGORY_DETAILS = "/supplier-central/category-details",
   EXPORT_SUPPLIER_CENTRAL_SHEET = "/supplier-central/export-sheet",
-SEARCH_SUPPLIERS = "/supplier-central/search",
-SEND_DIGEST = "/supplier-central/send-digest",
+  SEARCH_SUPPLIERS = "/supplier-central/search",
+  SEND_DIGEST = "/supplier-central/send-digest",
   // excel data download endpoints
   GET_DOWNLOAD_VEHICLE_DATA = "/vehicle/download/excel",
   GET_DOWNLOAD_COMPANY_DATA = "/company/download/excel",
