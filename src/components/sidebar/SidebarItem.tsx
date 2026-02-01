6fae0ccc194a49c0ed7e4786095367160d42a3c4
Ajesh <ajeshs.dev@gmail.com>
2026-02-01 11:58:14 +0530

    feat: sidebar items feature status indication added

--- src/components/sidebar/SidebarItem.tsx
+++ src/components/sidebar/SidebarItem.tsx
@@ -1,26 +1,36 @@
 import { Link } from "react-router-dom";
-import { SidebarSubItemType } from "./sidebar-config";
+import { SidebarFeatureStatusType, SidebarSubItemType } from "./sidebar-config";
 
 type SidebarItemProps = {
   icon?: any; // Optional icon
   label: string;
   onClick?: () => void;
+  featureStatus?: SidebarFeatureStatusType;
 };
 
 // sidebar general item which acts like a link
-export const SidebarItem = ({ icon, label, onClick }: SidebarItemProps) => {
+export const SidebarItem = ({
+  icon,
+  label,
+  onClick,
+  featureStatus,
+}: SidebarItemProps) => {
   // Default class for regular sidebar options
   const classNames = `flex h-11 min-h-11 w-full items-center justify-start gap-2 whitespace-nowrap rounded-lg pl-2 w-full truncate text-ellipsis text-left cursor-pointer no-underline`;
 
   const Icon = icon;
 
   return (
-    <div className={`${classNames} `} onClick={onClick}>
-      {/* Render icon if available */}
+    <div className={`${classNames} relative`} onClick={onClick}>
       {Icon && <Icon className="text-xl" size={20} strokeWidth={3} />}
 
-      {/* Sidebar item label */}
-      <span className="">{label}</span>
+      <span className="truncate">{label}</span>
+
+      {featureStatus && (
+        <span className="pointer-events-none absolute right-2 top-3 rounded px-1.5 py-0.5 text-[9px] font-semibold leading-none text-yellow">
+          {featureStatus}*
+        </span>
+      )}
     </div>
   );
 };
@@ -29,9 +39,11 @@ export const SidebarItem = ({ icon, label, onClick }: SidebarItemProps) => {
 export const AccordionSidebarItem = ({
   item,
   isActive,
+  featureStatus,
 }: {
   item: SidebarSubItemType;
   isActive: boolean;
+  featureStatus?: SidebarFeatureStatusType;
 }) => {
   const activeClassNames = `${
     isActive ? "text-black font-semibold" : "text-gray-600 hover:text-gray-800 "
@@ -40,9 +52,15 @@ export const AccordionSidebarItem = ({
   return (
     <Link
       to={item.link}
-      className={`h-9 min-h-9 w-36 min-w-36 max-w-36 items-center justify-start gap-2 truncate text-ellipsis whitespace-nowrap rounded-lg pl-6 text-left text-sm no-underline ${activeClassNames}`}
+      className={`relative flex h-9 min-h-9 w-full max-w-[98%] items-center truncate rounded-lg pl-6 pr-6 text-left text-sm no-underline ${activeClassNames}`}
     >
-      &#187; &nbsp;{item.label}
+      &#187;&nbsp;
+      <span className="truncate">{item.label}</span>
+      {featureStatus && (
+        <span className="pointer-events-none absolute right-1 top-2 rounded px-1 text-[8px] font-semibold leading-none text-orange">
+          {featureStatus}
+        </span>
+      )}
     </Link>
   );
 };
