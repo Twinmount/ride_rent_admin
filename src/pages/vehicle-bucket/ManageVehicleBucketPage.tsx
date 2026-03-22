aa61c78801ea9c077301fa2046b08ce0aee9c87b
Ajesh <ajeshs.dev@gmail.com>
2026-03-22 22:29:42 +0530

    refactor: vehicle bucket page heading/subheading limit increased

--- src/pages/vehicle-bucket/ManageVehicleBucketPage.tsx
+++ src/pages/vehicle-bucket/ManageVehicleBucketPage.tsx
@@ -135,12 +135,8 @@ export default function ManageVehicleBucketPage() {
 
       <FloatingActionButton
         href={`/manage-vehicle-bucket/add`}
-        label={
-          totalItems >= 20
-            ? `Max Limit Reached (${totalItems})`
-            : "New Vehicle Bucket"
-        }
-        disabled={totalItems >= 20}
+        label={"New Vehicle Bucket"}
+        disabled={isLoading}
       />
     </PageLayout>
   );
